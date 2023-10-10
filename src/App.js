144cd0fab85cf21fd772a176ebb6c9fc6212f057
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-10 13:51:06 +0100

    Finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Cat } from './components/Cat';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const client = new QueryClient({
+		defaultOptions: {
+			queries: {
+				refetchOnWindowFocus: true
+			},
+		},
+	});
+
+	return (
+		<div className="App">
+			<QueryClientProvider client={client}>
+				<Cat />
+			</QueryClientProvider>
+		</div>
+	);
 }
 
 export default App;
